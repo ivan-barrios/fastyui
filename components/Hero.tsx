2f4eb27881b72409ca0e9ea82ca9ff37c53b0b0a
Ivan Barrios <ivanbar8112003@gmail.com>
2024-03-10 20:55:23 -0300

    Set up done

---
+++ components/Hero.tsx
@@ -0,0 +1,12 @@
+const Hero = () => {
+  return (
+    <section className="max-w-[1440px] w-full pt-[100px]">
+      <div className="w-full flex flex-col items-center text-center">
+        <h1>Title</h1>
+        <p>Subtitle</p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
